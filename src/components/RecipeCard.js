87a9c44bdf0207ea99edf94b601cb9df73cdbdbd
Beata Detkova <beata.detkova.jr@gmail.com>
2020-06-26 17:00:58 +0200

    moved from functional component to class component, added expand support

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const RecipeCard = ({ recipeName }) => {
+class RecipeCard extends Component {
+  constructor(props) {
+    super(props)
+    this.clickHandler = this.clickHandler.bind(this)
+    this.state = {
+      isExpanded: false
+    }
+  }
 
-  return (
-      <div className ='tc bg-light-green dib br3 pa3 ma2 grow ba bw2 b--black shadow-5 pointer'>
+  clickHandler () {
+    this.setState({ isExpanded: !this.state.isExpanded })
+  }
+  render () {
+    const detail = this.state.isExpanded ?
+    <div>
+      <h2>{this.props.recipeName}</h2>
+      <h2>expanded!</h2>
+    </div>
+    :
+    <h2>{this.props.recipeName}</h2>
+    return (
+      <div className ='tc bg-light-green dib br3 pa3 ma2 grow ba bw2 b--black shadow-5 pointer' onClick={this.clickHandler}>
         <div className="flex flex-column items-center justify-around" style={{width: '200px'}}>
-          <h2>{recipeName}</h2>
+          {detail}
         </div>
       </div>
     );
+  }
 }
 
 export default RecipeCard;
