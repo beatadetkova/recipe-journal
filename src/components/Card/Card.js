93d5df6d20b814510908a2a9f3bcf0771e7a60c6
Beata Detkova <beata.detkova.jr@gmail.com>
2020-09-23 19:18:59 +0200

    Linting

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,7 +13,7 @@ const Card = ({ name, foodImage }) => {
       className="tc bg-light-green dib br3 pa3 ma2 grow ba bw2 b--black shadow-5 pointer"
       onClick={routeChange}
     >
-      <img alt="category" src={foodImage} />
+      <img alt="categoryComponent" src={foodImage} />
       <div
         className="flex flex-column items-center justify-around"
         style={{ width: '200px' }}
